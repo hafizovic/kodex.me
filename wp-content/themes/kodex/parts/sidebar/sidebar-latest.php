6fc35c0a5a0b308967a7105152e11ae389d5914d
Hafizovic <mensur.hafizovic@gmail.com>
2024-08-23 01:45:45 +0200

    Neke sitnice valjda

--- wp-content/themes/kodex/parts/sidebar/sidebar-latest.php
+++
@@ -1,7 +0,0 @@
-<div class="zox-home-right-wrap zoxrel zox-sticky-side">
-    <div id="zox-side-wrap" class="zoxrel zox-divs">
-
-        <?php adsense('adsense_rest_sidebar2'); ?>
-
-    </div>
-</div>
